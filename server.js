efcbc362f4be3c35d69b67316c2aa6534accc541
Explomat <cabehok@inbox.ru>
2015-09-18 17:06:15 +0300

    Some changed

--- server.js
+++ server.js
@@ -31,58 +31,5 @@ function stringifyWT(obj) {
 	return outStr;
 }
 
-function getExt(fileName) {
-	if (fileName == undefined)
-		return '';
-	var index = fileName.indexOf('.');
-	return index == -1 ? '' : fileName.substr(index + 1, fileName.length - index);
-}
 
-function saveQuestion(queryObjects){
-	var question = eval("t="queryObjects.Body);
-}
-
-function uploadFile(queryObjects) {
-	try {
-		var name = queryObjects.Form.file_upload[0] + '';
-		var data = queryObjects.Form.file_upload;
-		var ext = getExt(name);
-
-		var userDoc = OpenDoc(UrlFromDocID(curUserID));
-		var docResource = OpenNewDoc( 'x-local://wtv/wtv_resource.xmd' ); 
-		docResource.TopElem.person_id = 6070152459256493433; 
-		docResource.TopElem.allow_unauthorized_download = true; 
-		docResource.TopElem.allow_download = true; 
-		docResource.TopElem.file_name = name;
-		docResource.TopElem.name = name;
-		docResource.TopElem.type = ext;
-		docResource.TopElem.person_fullname = userDoc.TopElem.lastname + ' ' + userDoc.TopElem.firstname + ' ' + userDoc.TopElem.middlename;
-		docResource.BindToDb();
-		docResource.TopElem.put_str(data, name); 
-		docResource.Save();
-		
-		return stringifyWT({
-			id: docResource.DocID,
-			name: name,
-			error: null
-		});
-			
-	}
-	catch(e){
-		alert(e);
-		return false;
-	}
-}
-
-function removeImage(queryObjects) {
-	var id = Int(queryObjects.id);
-	try {
-		DeleteDoc(UrlFromDocID(id));
-	}
-	catch(e) {
-		alert(e);
-		return false;
-	}
-	return true;
-}
 %>
